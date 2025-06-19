74a958e9b3aadddeb5c4825c99785f83772cb42e
Thilinacj <thilinachamaraj@gmail.com>
2025-06-19 08:56:26 +0530

    set secret keat to env file.✔

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config() 
 
 
 export function registerUser(req,res){
@@ -39,7 +42,7 @@ export function loginUser(req,res){
                     lastName : user.lastName,
                     email : user.email,
                     role : user.role
-                },"Thilina")
+                },process.env.JWT_SECRET)
                 res.json({message : "Login successful",token :token})
             }else{
                 res.status(401).json({error : "Login failed"})
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use((req,res,next)=>{
 
    if(token != null){
       token = token.replace("Bearer ","")
-      jwt.verify(token,"Thilina",(err,decoded)=>{
+      jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
         if(!err){
             req.user = decoded
         }

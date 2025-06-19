c0d610f777654af9ba13a79336e684d5352cbf81
Thilinacj <thilinachamaraj@gmail.com>
2025-06-19 16:18:36 +0530

    set review approve part.

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -76,5 +76,29 @@ export function deleteReview(req,res){
     
 }
 
+export function approveReview(req,res){
+    const email = req.params.email
 
+    if(req.user == null){
+        res.status(401).json({message : "Please login and try again"})
+        return
+    }
+
+    if(req.user.role =="admin"){
+        Review.updateOne(
+        {
+          email : email
+        }
+        ,{
+          isApproved : true 
+        }
+       ).then(()=>{
+        res.json({message: "Review approved successfully."})
+       }).catch(()=>{
+        res.status(500).json({error: "Review approval failed."})
+       })
+    }else{
+        res.status(403).json({message : "You are not and admin.Only admins can approve the reviews."})
+    }
+}
     
